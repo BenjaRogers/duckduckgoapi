bf6c15feadc5df27e8f89f69d96de8063f1e8b74
bsrogers2 <bsrogers2@my.waketech.edu>
2021-10-24 12:39:19 -0400

    search ddg for presidents name & test module

--- duck.py
+++ duck.py
@@ -1,13 +1,32 @@
 import requests
 
+pres_list = ("Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson", "Buren", "Harrison", "Tyler",
+"Polk","Taylor", "Fillmore","Pierce", "Buchanan","Lincoln", "Johnson","Grant", "Hayes","Garfield", "Arthur", "Cleveland",
+"Harrison", "Cleveland", "McKinley", "Roosevelt", "Taft", "Wilson", "Harding", "Coolidge", "Hoover", "Roosevelt",
+"Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush", "Clinton", "Bush", "Obama",
+"Trump", "Biden")
+
 prefix_url = 'https://api.duckduckgo.com/?q='
 search_term = 'presidents+of+the+united+states'
 postfix_url = '&format=json'
 url = prefix_url + search_term + postfix_url
 
-
+text_string = ''
+text_list = list()
 r = requests.get(url)
 
 response = r.json()
+related_topics = response['RelatedTopics']
 
-print(response['RelatedTopics'])
+for item in related_topics:
+    text_list.append(item['Text'])
+    text_string += ' ' + item['Text']
+
+print(text_list)
+for pres in pres_list:
+    print(pres)
+    if pres not in text_string:
+        print(pres + 'not in')
+
+
+# print(response['RelatedTopics'][0]['Text'])
--- test_duck.py
+++ test_duck.py
@@ -5,3 +5,32 @@ def test_ddg0():
     resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
     rsp_data = resp.json()
     assert "DuckDuckGo" in rsp_data["Heading"]
+
+
+def test_pres():
+    pres_list = (
+        "Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson", "Buren", "Harrison", "Tyler",
+        "Polk", "Taylor", "Fillmore", "Pierce", "Buchanan", "Lincoln", "Johnson", "Grant", "Hayes", "Garfield",
+        "Arthur",
+        "Cleveland",
+        "Harrison", "Cleveland", "McKinley", "Roosevelt", "Taft", "Wilson", "Harding", "Coolidge", "Hoover",
+        "Roosevelt",
+        "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush", "Clinton", "Bush",
+        "Obama",
+        "Trump", "Biden")
+    prefix_url = 'https://api.duckduckgo.com/?q='
+    search_term = 'presidents+of+the+united+states'
+    postfix_url = '&format=json'
+    url = prefix_url + search_term + postfix_url
+    text_string = ''
+    r = requests.get(url)
+    response = r.json()
+    related_topics = response['RelatedTopics']
+    for item in related_topics:
+        text_string += ' ' + item['Text']
+    for pres in pres_list:
+        print(pres)
+        assert pres in text_string
+
+
+test_pres()
